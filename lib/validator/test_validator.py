6bd5624166a50f4bea1f619cf425afe0051f4177
Scott Brown <scottbrown0001@gmail.com>
2015-02-01 07:59:29 -0800

    package restructure name changes

--- lib/validator/test_validator.py
+++ lib/validator/test_validator.py
@@ -1,5 +1,5 @@
 import unittest
-import Validator
+import validator
 
 
 class TestValidator(unittest.TestCase):
@@ -9,69 +9,69 @@ class TestValidator(unittest.TestCase):
         Test that initialization and check works properly for a variety
         of criteria and targets.
         """
-        validator = Validator.Validator(0)
+        validator_0 = validator.Validator(0)
 
         # Confirm that exactly one parameter is required.
-        self.assertRaises(TypeError, validator.check)
-        self.assertRaises(TypeError, validator.check, 1, 2)
+        self.assertRaises(TypeError, validator_0.check)
+        self.assertRaises(TypeError, validator_0.check, 1, 2)
 
         # Confirm that checks that should return True, do so.
-        self.assertTrue(validator.check(0))
-        self.assertTrue(validator.check(False))
+        self.assertTrue(validator_0.check(0))
+        self.assertTrue(validator_0.check(False))
 
         # Confirm that checks that should return False, do so.
-        self.assertFalse(validator.check(1))
-        self.assertFalse(validator.check(True))
-        self.assertFalse(validator.check('a'))
-        self.assertFalse(validator.check([]))
-        self.assertFalse(validator.check([1, 2, 3]))
-        self.assertFalse(validator.check(None))
+        self.assertFalse(validator_0.check(1))
+        self.assertFalse(validator_0.check(True))
+        self.assertFalse(validator_0.check('a'))
+        self.assertFalse(validator_0.check([]))
+        self.assertFalse(validator_0.check([1, 2, 3]))
+        self.assertFalse(validator_0.check(None))
 
     def test_validator_1(self):
         """
         Test that initialization and check works properly for a variety
         of criteria and targets.
         """
-        validator = Validator.Validator(1)
+        validator_1 = validator.Validator(1)
 
         # Confirm that exactly one parameter is required.
-        self.assertRaises(TypeError, validator.check)
-        self.assertRaises(TypeError, validator.check, 1, 2)
+        self.assertRaises(TypeError, validator_1.check)
+        self.assertRaises(TypeError, validator_1.check, 1, 2)
 
         # Confirm that checks that should return True, do so.
-        self.assertTrue(validator.check(1))
-        self.assertTrue(validator.check(True))
+        self.assertTrue(validator_1.check(1))
+        self.assertTrue(validator_1.check(True))
 
         # Confirm that checks that should return False, do so.
-        self.assertFalse(validator.check(None))
-        self.assertFalse(validator.check(0))
-        self.assertFalse(validator.check('a'))
-        self.assertFalse(validator.check([]))
-        self.assertFalse(validator.check([1, 2, 3]))
-        self.assertFalse(validator.check(False))
+        self.assertFalse(validator_1.check(None))
+        self.assertFalse(validator_1.check(0))
+        self.assertFalse(validator_1.check('a'))
+        self.assertFalse(validator_1.check([]))
+        self.assertFalse(validator_1.check([1, 2, 3]))
+        self.assertFalse(validator_1.check(False))
 
     def test_validator_none(self):
         """
         Test that initialization and check works properly for a variety
         of criteria and targets.
         """
-        validator = Validator.Validator(None)
+        validator_none = validator.Validator(None)
 
         # Confirm that exactly one parameter is required.
-        self.assertRaises(TypeError, validator.check)
-        self.assertRaises(TypeError, validator.check, 1, 2)
+        self.assertRaises(TypeError, validator_none.check)
+        self.assertRaises(TypeError, validator_none.check, 1, 2)
 
         # Confirm that checks that should return True, do so.
-        self.assertTrue(validator.check(None))
+        self.assertTrue(validator_none.check(None))
 
         # Confirm that checks that should return False, do so.
-        self.assertFalse(validator.check(1))
-        self.assertFalse(validator.check(0))
-        self.assertFalse(validator.check('a'))
-        self.assertFalse(validator.check([]))
-        self.assertFalse(validator.check([1, 2, 3]))
-        self.assertFalse(validator.check(True))
-        self.assertFalse(validator.check(False))
+        self.assertFalse(validator_none.check(1))
+        self.assertFalse(validator_none.check(0))
+        self.assertFalse(validator_none.check('a'))
+        self.assertFalse(validator_none.check([]))
+        self.assertFalse(validator_none.check([1, 2, 3]))
+        self.assertFalse(validator_none.check(True))
+        self.assertFalse(validator_none.check(False))
 
 
 if __name__ == "__main__":
