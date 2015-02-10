bb0785657eba0f2c1623db72ecbd43550e79018f
Scott Brown <scottbrown0001@gmail.com>
2015-02-09 16:00:50 -0800

    basic implementation work

--- lib/validator/test/test_validator.py
+++ lib/validator/test/test_validator.py
@@ -2,7 +2,66 @@ import unittest
 import validator
 
 
-class TestValidator(unittest.TestCase):
+class TestValidatorExceptions(unittest.TestCase):
+    """
+    Test the management and use of the class and instance level
+    pass_exceptions and fail_exceptions lists.
+    """
+    def test_instance_add_exceptions(self):
+        """
+        Test that exceptions can be added to an instance's exceptions
+        management lists unless one occurs already in the "other"
+        list, in which case a ValueError is raised.
+        """
+        a_validator = validator.Validator(1)
+
+        self.assertEqual(a_validator.pass_exceptions, [])
+        self.assertEqual(a_validator.fail_exceptions, [])
+
+        # Basic assignment to exception lists is OK.
+        a_validator.pass_exceptions = [IOError]
+        a_validator.fail_exceptions = [TabError]
+
+        self.assertEqual(a_validator.pass_exceptions, [IOError])
+        self.assertEqual(a_validator.fail_exceptions, [TabError])
+
+        # A second assignment to exception lists results in a list
+        # with two Exceptions.
+        a_validator.pass_exceptions = a_validator.pass_exceptions.append(
+            OSError
+            )
+        a_validator.fail_exceptions = a_validator.fail_exceptions.append(
+            NotImplementedError
+            )
+
+        self.assertEqual(len(a_validator.pass_exceptions), 2)
+        self.assertTrue(IOError in a_validator.pass_exceptions)
+        self.assertTrue(OSError in a_validator.pass_exceptions)
+
+        self.assertEqual(len(a_validator.fail_exceptions), 2)
+        self.assertTrue(TabError in a_validator.fail_exceptions)
+        self.assertTrue(NotImplementedError in a_validator.fail_exceptions)
+
+        # Trying to add an exception that's already in the "other" list
+        # raises a ValueError exception. Check when the list being modified
+        # was previously empty and when it was not; also make sure it doesn't
+        # do anything funky when ValueError is the duplicate exception in
+        # question.
+        a_validator.pass_exceptions = []
+        a_validator.fail_exceptions = [ValueError]
+
+        self.assertRaises(
+            # Hmm, this is a property, right? How to do this? Don't think 
+            # assertRaises will work, just have to wrap in try and check the
+            # result after.
+            )
+
+
+class TestValidatorCheck(unittest.TestCase):
+    """
+    Test that initialization and check works properly for a variety
+    of criteria and targets.
+    """
 
     def test_validator_0(self):
         """
@@ -74,7 +133,22 @@ class TestValidator(unittest.TestCase):
         self.assertFalse(validator_none.check(False))
 
 
+# Build a suite with all the tests
+# class AllTests(unittest.TestCase):
+class AllTests():
+    def suite(self):
+        suite1 = unittest.TestLoader().loadTestsFromTestCase(
+            TestValidatorCheck
+            )
+        suite2 = unittest.TestLoader().loadTestsFromTestCase(
+            TestValidatorExceptions
+            )
+        return unittest.TestSuite([suite1, suite2])
+
 if __name__ == "__main__":
     # unittest.main()
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestValidator)
+    # suite = unittest.TestLoader().loadTestsFromTestCase(TestValidatorCheck)
+    alltests = AllTests()
+    suite = alltests.suite()
     unittest.TextTestRunner(verbosity=3).run(suite)
+    print "Hello World!"
